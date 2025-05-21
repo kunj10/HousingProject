6f7be059e4df24d3d3b7dac32886d6fb00bff4a7
kunj10 <kunjpatel91012@gmail.com>
2025-05-21 14:56:04 +0530

    Add HTML, CSS, and JavaScript files for the Bengaluru home price prediction application; remove unnecessary .gitignore entry.

--- Server/server.py
+++ Server/server.py
@@ -20,6 +20,7 @@ def predict_home_price():
   response = jsonify({
     'estimated_price' : util.get_estimated_price(location,total_sqft,bhk,bath)
   })
+  response.headers.add('Access-Control-Allow-Origin', '*')
   return response
 
 if __name__ == "__main__":
